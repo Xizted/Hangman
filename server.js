f5f3caf1a521049f453e80ffd2b3dd356cffcee2
Xizted <xizted79@gmail.com>
2020-04-22 18:07:52 -0400

    Upload Heroku

---
+++ server.js
@@ -0,0 +1,16 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist/ahorcado'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
